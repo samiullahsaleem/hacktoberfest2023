51237e0bd05c34bc62178efdcf4aabf3f16ca329
Inam Ul Haq <111003676+inamtyulhaq@users.noreply.github.com>
2023-10-12 18:06:44 +0500

    Create Random Number Generator Game.c

---
+++ Random Number Generator Game.c
@@ -0,0 +1,8 @@
+//This is the program that I wrote while solving my first Post Lab 1.
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+  int randnum, usernum;
+  printf("Welcome to the Number Guessing Game!")
+  printf("Guess the number: ")
+}
