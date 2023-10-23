acbb73267f719a7382746a534a88cf05321490ac
Shivadubal26 <72974708+Shivadubal26@users.noreply.github.com>
2023-10-23 17:53:44 +0530

    Create Swapping.py

---
+++ Swapping.py
@@ -0,0 +1,15 @@
+#swap fn to swap first n last element of a list
+def swapList(newList):
+    size = len(newList)
+     
+    # Swapping 
+    temp = newList[0]
+    newList[0] = newList[size - 1]
+    newList[size - 1] = temp
+     
+    return newList
+     
+# Driver code
+newList = [12, 35, 9, 56, 24]
+ 
+print(swapList(newList))
